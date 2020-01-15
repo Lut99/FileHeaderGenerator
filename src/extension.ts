63897d42f9faa03f716819c8924b352094a004d9
Lut99 <mullertim99@gmail.com>
2020-01-15 15:39:34 +0100

    Finished the package!

--- src/extension.ts
+++ src/extension.ts
@@ -15,9 +15,22 @@ class CommentSet {
 	}
 }
 
-function get_editor() {
+function get_editor(): string {
 	let config = vscode.workspace.getConfiguration();
-	return config.get("auto-header-generator.username");
+	let data = config.get<string>("auto-header-generator.username");
+	if (data === undefined) {
+		return "anonymous";
+	}
+	return data;
+}
+
+function get_n_lines(): number {
+	let config = vscode.workspace.getConfiguration();
+	let data = config.get<number>("auto-header-generator.searchLines");
+	if (data === undefined) {
+		return 20;
+	}
+	return data;
 }
 
 function get_header_title(doc: vscode.TextDocument): string {
@@ -76,11 +89,11 @@ function get_comment_set(doc: vscode.TextDocument): CommentSet {
 	let id = doc.languageId;
 	// Use that for the comment character
 	if (id === "c" || id === "cpp" || id === "csharp" || id === "java") {
-		return new CommentSet("/* ", " * ", "**/");
+		return new CommentSet("/*", " *", "**/");
 	} else if (id === "python") {
-		return new CommentSet("# ", "# ", "# ");
+		return new CommentSet("#", "#", "#");
 	} else if (id === "lua") {
-		return new CommentSet("--[[ ", "     ", "--]]");
+		return new CommentSet("--[[", "    ", "--]]");
 	} else {
 		return new CommentSet("", "", "");
 	}
@@ -91,14 +104,40 @@ function get_now(): string {
 	return now.toLocaleString();
 }
 
-function generate_header(): void {
-	// Fetch the currently opened document
-	let doc = vscode.window.activeTextEditor?.document;
-	if (doc === undefined) {
-		vscode.window.showErrorMessage("No file opened");
-		return;
+function wrap_description(description: string, line_start: string, line_length: number = 79) {
+	let description_words = description.split(" ");
+	let wrapped_description = "";
+	let line = line_start;
+	let functional_length = line_length - line_start.length;
+	while (description_words.length > 0) {
+		let word = description_words.shift()!;
+		// Check if we can add this word to the line without clipping. If we
+		//   can't, begin a newline. Only do this if there is something in the
+		//   line buffer.
+		if (line.length > line_start.length && line.length + word.length > functional_length) {
+			// Do a newline first
+			wrapped_description += line + "\n";
+			line = line_start;
+		}
+		// Check if the word itself will be able to fit. If not, then split the
+		//   word in two and add it to the list as first words
+		if (word.length > functional_length) {
+			description_words = [word.substr(0, functional_length), word.substr(functional_length)].concat(description_words);
+		} else {
+			// If everything got through correctly, add the current word to the line
+			if (line.length > line_start.length) {
+				line += " ";
+			}
+			line += word;
+		}
 	}
 
+	// Add the line as final one
+	wrapped_description += line + "\n";
+	return wrapped_description;
+}
+
+function generate_header(doc: vscode.TextDocument, description: string): void {
 	// Determine the path of the currently opened document
 	let path = doc.uri;
 
@@ -107,19 +146,23 @@ function generate_header(): void {
 
 	// Fetch the filename (with extension)
 	let title = get_header_title(doc);
+
+	// Wrap the description if necessary
+	let description_wrapped = wrap_description(description, set.middle + "   ");
 	
 	// Create the full comment text
-	let text = set.start + title + "\n";
-	text += set.middle + "  by " + get_editor() + "\n";
+	let text = set.start + " " + title + "\n";
+	text += set.middle + "   by " + get_editor() + "\n";
 	text += set.middle + "\n";
-	text += set.middle + "Created:\n";
-	text += set.middle + "  " + get_now() + "\n";
-	text += set.middle + "Last edited:\n";
-	text += set.middle + "  " + get_now() + "\n";
-	text += set.middle + "Auto updated?\n";
-	text += set.middle + "  Yes\n";
+	text += set.middle + " Created:\n";
+	text += set.middle + "   " + get_now() + "\n";
+	text += set.middle + " Last edited:\n";
+	text += set.middle + "   " + get_now() + "\n";
+	text += set.middle + " Auto updated?\n";
+	text += set.middle + "   Yes\n";
 	text += set.middle + "\n";
-	text += set.middle + "Test description\n";
+	text += set.middle + " Description:\n";
+	text += description_wrapped;
 	text += set.end + "\n\n";
 
 	// Create an edit
@@ -128,22 +171,150 @@ function generate_header(): void {
 	vscode.workspace.applyEdit(edit);
 }
 
-/* Event listener for when a user saves a file, i.e. the header should be updated. */
-function update_header(): void {
+async function prepare_generation() {
+	// Fetch the currently opened document
+	let doc = vscode.window.activeTextEditor?.document;
+	if (doc === undefined) {
+		vscode.window.showErrorMessage("No open file");
+		return;
+	}
 
+	// Query the user about a description
+	let description = await vscode.window.showInputBox({
+		placeHolder: "e.g., Python Class that describes a dog...",
+		prompt: "File description"
+	});
+	if (description === undefined) {
+		return;
+	} else if (description === "") {
+		description = "<Todo>";
+	}
+
+	// Do the actual generation
+	generate_header(doc, description);
+}
+
+function get_line(text: string): string | undefined {
+	let to_return = "";
+	for (var i = 0; i < text.length; i++) {
+		if (text[i] === "\n") {
+			// Done, return the line
+			return to_return;
+		}
+		to_return += text[i];
+	}
+	
+	// No newline was found, return the remaining text unless no text was found at all
+	if (to_return === "") {
+		return undefined;
+	}
+	return to_return;
+}
+
+function remove_spaces(text: string): string {
+	let to_return = "";
+	for (let i = 0; i < text.length; i++) {
+		if (text[i] !== " ") {
+			to_return += text[i];
+		}
+	}
+	return to_return;
+}
+
+/* Event listener for when a user saves a file, i.e. the header should be updated. */
+var can_update = true;
+function update_header(doc: vscode.TextDocument): void {
+	if (!can_update) {
+		return;
+	}
+
+	// Fetch the comment set
+	let set = get_comment_set(doc);
+
+	// Fetch the maximum number of lines we'll search
+	let N = get_n_lines();
+
+	// Simply search the first N lines for the line: set.middle + " Auto updated?"
+	// But while at it, also save position of line: set.middle + " Last edited:"
+	let text_to_search = doc.getText();
+	let auto_updated = "unknown";
+	let last_edited_pos = -1;
+	let last_edited_length;
+	for (let i = 0; i < N; i++) {
+		let raw_line = get_line(text_to_search);
+		if (raw_line === undefined) {
+			break;
+		}
+		text_to_search = text_to_search.substr(raw_line.length + 1);
+
+		// Check if it starts with the middle char and remove it if so
+		if (raw_line.substr(0, set.middle.length) !== set.middle) {
+			continue;
+		}
+		let line = raw_line.substr(set.middle.length);
+
+		// Then, remove all spaces
+		line = remove_spaces(line);
+
+		// Check if it's one of the lines we want
+		if (auto_updated === "pending") {
+			let lower_line = line.toLowerCase();
+			if (lower_line === "yes" || lower_line === "no") {
+				auto_updated = lower_line;
+			} else {
+				vscode.window.showErrorMessage("Unknown auto-update option in header; should be 'yes' or 'no'");
+				return;
+			}
+		} else if (last_edited_pos !== -1 && last_edited_length === undefined) {
+			last_edited_length = raw_line.length;
+		} else if (line === "Autoupdated?") {
+			auto_updated = "pending";
+		} else if (line === "Lastedited:") {
+			last_edited_pos = i;
+		}
+	}
+
+	// Check what we have
+	if (auto_updated === "unknown" || auto_updated === "no") {
+		// No auto update enabled
+		console.log("auto-header-updater: no auto update enabled for file: \"" + doc.uri.path + "\"");
+		return;
+	} else if (auto_updated === "pending") {
+		vscode.window.showErrorMessage("Auto updated should have a line following with 'yes' or 'no'");
+		return;
+	}
+
+	// If auto updateing but no last_edited found
+	if (last_edited_pos === -1 || last_edited_length === undefined) {
+		vscode.window.showErrorMessage("Header is auto updated but not last edited field found");
+		return;
+	}
+
+	// Now that everything's correct, update the last edited field
+	let edit = new vscode.WorkspaceEdit();
+	edit.replace(doc.uri, new vscode.Range(new vscode.Position(last_edited_pos + 1, 0), new vscode.Position(last_edited_pos + 1, last_edited_length)), set.middle + "   " + get_now());
+	let edit_resolve = vscode.workspace.applyEdit(edit);
+	edit_resolve.then(() => {
+		can_update = false;
+		let save_resolve = doc.save();
+		save_resolve.then(() => {
+			can_update = true;
+		});
+	});
+
+	console.log("auto-header-updater: update success for file: \"" + doc.uri.path + "\"");
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.generateHeader', generate_header);
-	//let another_disposable = vscode.workspace.onDidSaveTextDocument(update_header);
+	let disposable = vscode.commands.registerCommand('extension.generateHeader', prepare_generation);
+	let another_disposable = vscode.workspace.onDidSaveTextDocument(update_header);
 
-	context.subscriptions.push(disposable);//, another_disposable);
+	context.subscriptions.push(disposable, another_disposable);
 }
 
 // this method is called when your extension is deactivated
--- test_space/testCase.py
+++ test_space/testCase.py
@@ -1,3 +1,17 @@
+# TEST CASE.py
+#   by Lut99
+#
+# Created:
+#   1/15/2020, 3:35:16 PM
+# Last edited:
+#   1/15/2020, 3:39:06 PM
+# Auto updated?
+#   Yes
+#
+# Description:
+#   Test
+#
 
 
-print ("Hello there!")
+
+print("A new update has occurred...")
